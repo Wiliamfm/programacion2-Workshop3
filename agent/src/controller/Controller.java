253329f2130b7ef1eaf6832d47a65eebc6f0a296
William Fonseca <santifon15@gmail.com>
2021-04-04 08:49:12 -0500

    add refused connection option

--- agent/src/controller/Controller.java
+++ agent/src/controller/Controller.java
@@ -16,22 +16,25 @@ public class Controller {
     System.out.println("Bienvenido, espere hasta que un cliente solicite conexión");
     sc = new Scanner(System.in);
     a = new Agent();
-    a.serverChat();
-    if (a.message.equals("message-..-")) {
-      System.out.println("¿Desea conectarse con el cliente? \n1. Aceptar \n2. Denegar");
-      switch (sc.nextLine()) {
-      case "1":
-        a.sendMessage("AceptaR-..-");
-        a.start();
-        while (a.isAlive()) {
-          a.sendMessage(sc.nextLine());
+    while (true) {
+      a.serverChat();
+      if (a.message.equals("message-..-")) {
+        System.out.println("¿Desea conectarse con el cliente? \n1. Aceptar \n2. Denegar");
+        switch (sc.nextLine()) {
+        case "1":
+          a.sendMessage("AceptaR-..-");
+          a.start();
+          while (a.isAlive()) {
+            a.sendMessage(sc.nextLine());
+          }
+        case "2":
+          System.out.println("No se estableció conexión");
+          a.sendMessage("DeneGado-..-");
+          break;
+        default:
+          System.out.println("Entrada no válida");
+          break;
         }
-      case "2":
-        System.out.println("No se estableció conexión");
-        break;
-      default:
-        System.out.println("Entrada no válida");
-        break;
       }
     }
   }
--- server/src/controller/Controller.java
+++ server/src/controller/Controller.java
@@ -1,21 +1,14 @@
 package controller;
 
-import model.ChatServer;
 import model.Server;
 
 public class Controller {
 
   private Server server;
-  private ChatServer cServer;
 
   public Controller() {
     server = new Server();
-    cServer = new ChatServer();
     Thread serverT = new Thread(server);
     serverT.start();
-    // Chat Server
-    Thread cServerT = new Thread(cServer);
-    cServerT.start();
   }
-
 }
