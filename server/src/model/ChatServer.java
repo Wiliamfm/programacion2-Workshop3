528cf95e76a78a60b91892f0c8c552afafba8d07
William Fonseca <santifon15@gmail.com>
2021-04-04 18:59:55 -0500

    ?

---
+++ server/src/model/ChatServer.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer implements Runnable {
+
+  public ServerSocket servidor;
+
+  @Override
+  public void run() {
+    try {
+      servidor = new ServerSocket(1001);
+      System.out.println("Chat Server created.");
+      while (true) {
+        Socket s = servidor.accept();
+        DataInputStream inputData = new DataInputStream(s.getInputStream());
+        String a = inputData.readUTF();
+        System.out.println("Entrada al chat: " + a);
+        Socket send;
+        DataOutputStream sendData;
+        String[] m = a.split(",");
+        switch (m[2]) {
+        case "c":
+          send = new Socket(InetAddress.getLocalHost().getHostAddress(), 1002);
+          sendData = new DataOutputStream(send.getOutputStream());
+          sendData.writeUTF(m[1]);
+          break;
+        case "a":
+          send = new Socket(InetAddress.getLocalHost().getHostAddress(), 1003);
+          sendData = new DataOutputStream(send.getOutputStream());
+          sendData.writeUTF(m[1]);
+          break;
+        default:
+          break;
+        }
+        inputData.close();
+        s.close();
+      }
+    } catch (IOException e) {
+      System.out.println("Cannot create chat server Socket: " + e.getMessage());
+    }
+  }
+
+}
